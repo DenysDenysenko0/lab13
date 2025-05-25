31ec6bc99e527281497795b8906724f53274fc59
AlexKubusha <kubusha.oleksii@student.sumdu.edu.ua>
2025-05-25 13:36:08 +0300

    Додано код Кубуши

--- 1.py
+++ 1.py
@@ -39,3 +39,39 @@ except FileNotFoundError:
     print("CSV файл не знайдено")
 
 # Кінець коду Денисенка Дениса
+
+# Код Кубуши Олексія (Зчитування JSON файлу Дениса, додавання нових рядків та запис у новий CSV файл)
+# Імена файлів
+csv_file1 = "kubcsv.csv"
+json_file1 = "danyjson.json"
+
+# Дані для запису
+dany1 = [
+    {"Ім'я": "Кубуша", "Предмет": "Вища математика", "Оцінка": 92},
+    {"Ім'я": "Кубуша", "Предмет": "Дискретна математика", "Оцінка": 100},
+    {"Ім'я": "Кубуша", "Предмет": "Програмування", "Оцінка": 98},
+]
+
+# Читання з JSON файлу
+try:
+    with open(json_file1, mode='r', encoding='utf-8') as file:
+        rows = json.load(file)
+except FileNotFoundError:
+    print("JSON файл Дениса не знайдено")
+    rows = []
+
+# Додаємо нові рядки
+rows.extend(dany1)
+print(f"Нові рядки додано")
+
+# Запис у CSV файл
+try:
+    with open(csv_file1, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=["Ім'я", "Предмет", "Оцінка"])
+        writer.writeheader()
+        writer.writerows(rows)
+    print("CSV файл Кубуши створено")
+except Exception as e:
+    print(f"Помилка при записуванні у CSV файл: {e}")
+
+# Кінець коду Кубуши Олексія
